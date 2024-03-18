dfad760777e6c5ffd59ef2693498a90002ec7744
Christian Clausen <maxclausen@gmail.com>
2024-03-18 21:20:04 +0100

    Initial commit

---
+++ Comfily/src/app/home/home.component.ts
@@ -0,0 +1,38 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
+  timeout: ReturnType<typeof setTimeout> | undefined;
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+  delayedFilter(text: string) {
+    if (this.timeout !== undefined) clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => this.filterResults(text), 500);
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# Angular Strategy
+
+## Background
+We have made a home listing app in Angular. We have already gotten three customers who want to use it:
+
+- WeLive
+- Comfily
+- BlockchainHomes.ai
+
+However, each customer had a few changes they wanted, so now we have ended up with three (almost identical) copies of the codebase. This is hard to maintain, so we would like to get it down to one codebase!
+
+## Objective
+Your task, should you choose to accept it, is to:
+
+1. Identify the differences between the codebases.
+2. Use Strategy Pattern to unify the two most similar codebases.
+3. Use Strategy Pattern to unify the two final codebases.
+4. Make Abstract Factories for the three customers.
+
+## Learning Goals
+
+* Ensemble programming
+* String-style pairing
+* Strategy pattern
+* Abstract Factory pattern
+
+## How to run it
+
+To run it use the commands:
+
+1. `cd [folder]`
+2. `npm install --legacy-peer-deps`
+3. `npm start`
+4. Then open a browser and go to `http://localhost:4200`
+
